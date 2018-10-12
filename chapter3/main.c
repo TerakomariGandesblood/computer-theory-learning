a56bf61865f2fabf87c33f074097b798604ec67b
kaiser <1244713586@qq.com>
2018-10-12 21:55:32 +0800

    2018.10.12

--- chapter3/main.c
+++ chapter3/main.c
@@ -1,6 +1,6 @@
-long mult2(long ,long);
+long mult2(long, long);
 
-void multstore(long x,long y,long *dest){
-    long t=mult2(x,y);
-    *dest=t;
+void multstore(long x, long y, long *dest) {
+    long t = mult2(x, y);
+    *dest = t;
 }
