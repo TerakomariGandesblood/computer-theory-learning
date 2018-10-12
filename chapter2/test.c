826483641e16ca2768d7e114f4b41a698ab85772
kaiser <1244713586@qq.com>
2018-10-12 20:13:10 +0800

    2018.10.11

--- chapter2/test.c
+++ chapter2/test.c
@@ -5,11 +5,9 @@
 #include "chapter2.h"
 #include <stdio.h>
 #include <assert.h>
+#include <math.h>
+#include <float.h>
 
 int main(void) {
-    assert(float_le(-0, +0));
-    assert(float_le(+0, -0));
-    assert(float_le(0, 3));
-    assert(float_le(-4, -0));
-    assert(float_le(-4, 4));
+
 }
