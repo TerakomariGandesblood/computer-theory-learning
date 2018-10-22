747a9e0e71fabf4d4963f8bb7b5ed45c08437560
kaiser <1244713586@qq.com>
2018-10-22 22:01:14 +0800

    2018.10.22

--- chapter3/test/main.c
+++ chapter3/test/main.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main() {
-    __asm__(
-    "movb $0xf,(%ebx);");
-    printf("%ld\n");
+// int main() {
+//     __asm__(
+//     "movb $0xf,(%ebx);");
+//     printf("%ld\n");
+// }
+
+long fuck(long x, long y, long z) {
+    long t = x + 4 * y + 12 * z;
+    return t;
 }
--- chapter3/test/main.s
+++ chapter3/test/main.s
@@ -1,30 +1,17 @@
 	.file	"main.c"
 	.text
-	.section	.rodata.str1.1,"aMS",@progbits,1
-.LC0:
-	.string	"%ld\n"
-	.text
-	.globl	main
-	.type	main, @function
-main:
-.LFB3:
+	.globl	fuck
+	.type	fuck, @function
+fuck:
+.LFB0:
 	.cfi_startproc
-	subq	$8, %rsp
-	.cfi_def_cfa_offset 16
-#APP
-# 4 "main.c" 1
-	movb $0xf,(%ebx);
-# 0 "" 2
-#NO_APP
-	movl	$.LC0, %edi
-	movl	$0, %eax
-	call	printf
-	movl	$0, %eax
-	addq	$8, %rsp
-	.cfi_def_cfa_offset 8
+	leaq	(%rdi,%rsi,4), %rax
+	leaq	(%rdx,%rdx,2), %rcx
+	leaq	0(,%rcx,4), %rdx
+	addq	%rdx, %rax
 	ret
 	.cfi_endproc
-.LFE3:
-	.size	main, .-main
+.LFE0:
+	.size	fuck, .-fuck
 	.ident	"GCC: (GNU) 8.2.0"
 	.section	.note.GNU-stack,"",@progbits
