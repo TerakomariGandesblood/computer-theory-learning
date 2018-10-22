e95bc3e3a4a49c640efe75d1cb916f0f1573eb70
kaiser <1244713586@qq.com>
2018-10-22 21:05:46 +0800

    2018.10.22

--- chapter3/main.asm
+++ chapter3/main.asm
@@ -6,7 +6,6 @@
 _start:
     movl %eax,%rax
 
-
-    movq $0,%rbx     # 参数一：退出代码
-    movq $1,%rax     # 系统调用号(sys_exit)
-    int  $0x80       # 调用内核功能
+    movq $0,%rbx
+    movq $1,%rax
+    int  $0x80
