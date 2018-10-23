3146afcb6cb1c6e92ed50283922d0c0c4743cd05
kaiser <1244713586@qq.com>
2018-10-23 21:48:57 +0800

    2018.10.23

--- chapter3/test/main.c
+++ chapter3/test/main.c
@@ -1,12 +1,7 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     __asm__(
-//     "movb $0xf,(%ebx);");
-//     printf("%ld\n");
-// }
-
-long fuck(long x, long y, long z) {
-    long t = x + 4 * y + 12 * z;
-    return t;
+int main() {
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", i);
+    }
 }
--- chapter3/test/main.s
+++ chapter3/test/main.s
@@ -1,17 +1,34 @@
 	.file	"main.c"
 	.text
-	.globl	fuck
-	.type	fuck, @function
-fuck:
-.LFB0:
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.LC0:
+	.string	"%d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB3:
 	.cfi_startproc
-	leaq	(%rdi,%rsi,4), %rax
-	leaq	(%rdx,%rdx,2), %rcx
-	leaq	0(,%rcx,4), %rdx
-	addq	%rdx, %rax
+	pushq	%rbx
+	.cfi_def_cfa_offset 16
+	.cfi_offset 3, -16
+	movl	$0, %ebx
+.L2:
+	cmpl	$9, %ebx
+	jg	.L5
+	movl	%ebx, %esi
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	printf
+	addl	$1, %ebx
+	jmp	.L2
+.L5:
+	movl	$0, %eax
+	popq	%rbx
+	.cfi_def_cfa_offset 8
 	ret
 	.cfi_endproc
-.LFE0:
-	.size	fuck, .-fuck
+.LFE3:
+	.size	main, .-main
 	.ident	"GCC: (GNU) 8.2.0"
 	.section	.note.GNU-stack,"",@progbits
