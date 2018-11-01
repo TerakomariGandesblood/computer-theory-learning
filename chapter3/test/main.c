9cc42cf1fcf7c7109338d12e83c6c4016035ff83
kaiser <1244713586@qq.com>
2018-11-01 14:59:18 +0800

    2018.11.1

--- chapter3/test/main.c
+++ chapter3/test/main.c
@@ -1,6 +1,21 @@
-#include <stdio.h>
+void fuck(long x, long n, long *dest) {
+    long val = x;
 
-int main() {
-    __asm__(
-    "xorl %eax,%eax;");
+    switch (n) {
+        case 100:
+            val *= 13;
+            break;
+        case 102:
+            val += 10;
+        case 103:
+            val += 11;
+            break;
+        case 104:
+        case 106:
+            val *= val;
+            break;
+        default:
+            val = 0;
+    }
+    *dest = val;
 }
